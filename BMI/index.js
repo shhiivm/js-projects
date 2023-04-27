4722335cc50e7f48a6ef4f58d6b504e053e903ec
shhiivm <shhiivm@pm.me>
2023-04-27 16:38:25 +0530

    Now users can enter their names in BMI

--- BMI/index.js
+++ BMI/index.js
@@ -1,19 +1,20 @@
-var weight, height, measure, bmi, data;
+var weight, height, bmi, data;
 
 function calculate() {
+  userName = document.getElementById('name').value;
   weight = document.getElementById("weight").value;
   height = document.getElementById("height").value;
 
   heigth = 0.01 * height;
   bmi = (weight / (heigth * heigth)).toFixed(1);
   if (bmi <= 18.5) {
-    data = `Your BMI is ${bmi} and You are UnderWeight`;
+    data = `${userName}'s BMI is ${bmi} and You are UnderWeight`;
   } else if (bmi >= 18.6 && bmi <= 24.9) {
-    data = `Your BMI is ${bmi} and You are Healthy`;
+    data = `${userName}'s BMI is ${bmi} and You are Healthy`;
   } else if ((bmi) => 25 && bmi <= 29.9) {
-    data = `Your BMI is ${bmi} and You are Overweight`;
+    data = `${userName}'s BMI is ${bmi} and You are Overweight`;
   } else if (bmi > 30) {
-    data = `Your BMI is ${bmi} and You are Obese`;
+    data = `${userName}'s BMI is ${bmi} and You are Obese`;
   }
 
   if (weight < 1 || height < 1) {
